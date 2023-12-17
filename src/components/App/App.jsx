b6b249b255e3f6fc5dfbee7fed6e732320f1eb81
Ruslan <120032162+INRGI@users.noreply.github.com>
2023-12-17 14:01:26 +0100

    Add toastify

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,11 @@
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1>React homework template</h1>
+      <ToastContainer />
     </div>
   );
 };
